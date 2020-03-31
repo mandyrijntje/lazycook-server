b62413595b76ceba1f35f8f3c61498211da6aaef
messmonte <amandamariamonte@gmail.com>
2020-03-31 15:47:35 +0200

    Added POST/EDIT function to recipe router

--- recipe/router.js
+++ recipe/router.js
@@ -1,7 +1,8 @@
 const express = require("express");
-// const { Op } = require("sequelize");
-
+const auth = require("../auth/middleware");
 const Recipe = require("./model");
+const Ingredient = require("../ingredient/model");
+const RecipeIngredient = require("../recipeIngredient/model");
 const router = express.Router();
 
 // get all recipes
@@ -10,11 +11,6 @@ router.get("/recipe", (request, response, next) => {
   const offset = request.query.offset || 0;
   try {
     Recipe.findAndCountAll({
-      // where: {
-      //   startDate: {
-      //     [Op.gte]: new Date()
-      //   }
-      // },
       limit,
       offset
     }).then(result => {
@@ -26,39 +22,66 @@ router.get("/recipe", (request, response, next) => {
   }
 });
 
-// // post an ingredient
+//POST many-to-many steps that I've used
+
+// 1. request.body.ingredients = [{"id":2}, {"id": 5}, {"id": 6}] 
+//(or whatever your new object needs to have)
+
+// 2. httpie without ingredients
+// http :4000/recipe name="ee" imageUrl="rr" step1='ee' isVegan='true' isVegetarian='true' hasNuts='false' hasDairy='false' 
+
+// 3. https://stackoverflow.com/questions/37215565/sending-nested-json-object-using-httpie
+
+// 4. httpie WITH ingredients 
+// http :4000/recipe name="ee" imageUrl="rr" step1='ee' isVegan='true' isVegetarian='true' hasNuts='false' hasDairy='false' ingredients:='[{"id":2}, {"id":5}]'
+
+// 5. Adjusting optionalIngredient as needed in PostgresQL database
+
 router.post("/recipe", async (request, response, next) => {
   try {
-    const {
-      name,
-      imageUrl,
-      step1,
-      step2,
-      step3,
-      step4,
-      step5,
-      step6,
-      isVegan,
-      isVegetarian,
-      hasNuts,
-      hasDairy
-    } = request.body;
-    const entity = {
-      name,
-      imageUrl,
-      step1,
-      step2,
-      step3,
-      step4,
-      step5,
-      step6,
-      isVegan,
-      isVegetarian,
-      hasNuts,
-      hasDairy
-    };
-    const recipe = await Recipe.create(entity);
-    response.send(recipe);
+    // 1. Recipe (name, imageUrl, ...)
+    const savedRecipe = await Recipe.create(
+      request.body
+    );
+
+    // 2. Finding these -> Ingredient (name, imageUrl, ...)
+
+    // So step (1), finding all the ingredients
+    // ingredientsPromises = [Promise, Promise, ...]
+    const ingredientsPromises = request.body.ingredients.map(async item => {
+      const ingredient = await Ingredient.findByPk(item.id);
+      return ingredient;
+    });
+
+    // // ingredientsPromise = Promise of [ingr, ingr, ...]
+    const ingredientsPromise = Promise.all(ingredientsPromises);
+
+    // // ingredients = [ingr, ingr, ...]
+    const ingredients = await ingredientsPromise;
+
+    // throwing an error if one of them is null
+    for (const ingredient of ingredients) {
+      if (!ingredient) {
+        return response.status(400).json({ error: "UFI: Unidentified flying ingredient" });
+      }
+    }
+
+    // 3. making these -> RecipeIngredient (recipeId, ingredientId)
+    // An array of Promises
+    const listOfPromises = ingredients.map(async ingredient => {
+      const recipeingredient = {
+        optionalIngredient: false,
+        recipeId: savedRecipe.id,
+        ingredientId: ingredient.id
+      };
+      // RecipeIngredient <- { recipeId, ingredientId }
+      await RecipeIngredient.create(recipeingredient);
+    });
+
+    // Now we have one big Promise of an array
+    await Promise.all(listOfPromises);
+
+    return response.status(200).json(savedRecipe);
   } catch (error) {
     next(error);
   }
@@ -90,6 +113,38 @@ router.get("/recipe/:id/ingredient", async (request, response, next) => {
   }
 });
 
-module.exports = router;
+//update a recipe for a user
+router.put("/recipe/:recipeId", auth, (request, response, next) => {
+  Recipe.findOne({
+    where: {
+      recipeId: request.params.recipeId
+    }
+  })
+    .then(recipe => {
+      if (recipe) {
+        recipe.update(request.body).then(recipe => response.json(recipe));
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(next);
+});
+
+//delete a recipe for a user
+router.delete("/recipe/:recipeId", auth, (request, response, next) => {
+  Recipe.destroy({
+    where: {
+      recipeId: request.params.recipeId
+    }
+  })
+    .then(numDeleted => {
+      if (numDeleted) {
+        response.status(204).end();
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch(next);
+});
 
 module.exports = router;
