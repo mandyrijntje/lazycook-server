5ae5e549d2e861edd4d88ae47a854b58ca0ac721
messmonte <amandamariamonte@gmail.com>
2020-06-09 22:46:26 +0200

    Setting high limit to show all recipes

--- recipe/router.js
+++ recipe/router.js
@@ -7,14 +7,14 @@ const router = express.Router();
 
 // get all recipes
 router.get("/recipe", (request, response, next) => {
-  const limit = Math.min(request.query.limit || 10, 50);
+  const limit = Math.min(request.query.limit || 50, 100);
   const offset = request.query.offset || 0;
   try {
     Recipe.findAndCountAll({
       include: [Ingredient],
       limit,
-      offset
-    }).then(result => {
+      offset,
+    }).then((result) => {
       return response.send({ recipes: result.rows, total: result.count });
     });
   } catch (error) {
@@ -42,14 +42,14 @@ router.post("/users/:userId/recipe", auth, async (request, response, next) => {
     // 1. Recipe (name, imageUrl, ...)
     const savedRecipe = await Recipe.create({
       ...request.body,
-      userId: request.user.dataValues.id
+      userId: request.user.dataValues.id,
     });
 
     // 2. Finding these -> Ingredient (name, imageUrl, ...)
 
     // So step (1), finding all the ingredients
     // ingredientsPromises = [Promise, Promise, ...]
-    const ingredientsPromises = request.body.ingredients.map(async item => {
+    const ingredientsPromises = request.body.ingredients.map(async (item) => {
       const ingredient = await Ingredient.findByPk(item.id);
       return ingredient;
     });
@@ -71,11 +71,11 @@ router.post("/users/:userId/recipe", auth, async (request, response, next) => {
 
     // 3. making these -> RecipeIngredient (recipeId, ingredientId)
     // An array of Promises
-    const listOfPromises = ingredients.map(async ingredient => {
+    const listOfPromises = ingredients.map(async (ingredient) => {
       const recipeingredient = {
         optionalIngredient: false,
         recipeId: savedRecipe.id,
-        ingredientId: ingredient.id
+        ingredientId: ingredient.id,
       };
       // RecipeIngredient <- { recipeId, ingredientId }
       await RecipeIngredient.create(recipeingredient);
@@ -86,7 +86,7 @@ router.post("/users/:userId/recipe", auth, async (request, response, next) => {
 
     const updatedRecipes = await Recipe.findAll({
       where: { userId: request.user.dataValues.id },
-      include: [Ingredient]
+      include: [Ingredient],
     });
     response.send(updatedRecipes);
   } catch (error) {
@@ -100,7 +100,7 @@ router.get("/recipe/:id", async (request, response, next) => {
     const { id } = request.params;
     const recipe = await Recipe.findOne({
       where: { id: request.params.id },
-      include: [Ingredient]
+      include: [Ingredient],
     });
     response.send(recipe);
   } catch (error) {
@@ -129,31 +129,30 @@ router.put("/recipe/:id", auth, async (request, response, next) => {
   try {
     const foundRecipe = await Recipe.findOne({
       where: {
-        id: request.params.id
-      }
+        id: request.params.id,
+      },
     });
     const updatedRecipe = await foundRecipe.update({
       ...request.body,
-      userId: request.user.dataValues.id
+      userId: request.user.dataValues.id,
     });
 
     const deletedRIs = await RecipeIngredient.destroy({
       where: {
-        recipeId: request.params.id
-      }
+        recipeId: request.params.id,
+      },
     });
-    request.body.ingredients.map(item => {
+    request.body.ingredients.map((item) => {
       RecipeIngredient.create({
         optionalIngredient: false,
         recipeId: request.params.id,
-        ingredientId: item.id
+        ingredientId: item.id,
       });
     });
 
-
     const updatedRecipes = await Recipe.findAll({
       where: { userId: request.user.dataValues.id },
-      include: [Ingredient]
+      include: [Ingredient],
     });
 
     response.send(updatedRecipes);
@@ -167,10 +166,10 @@ router.delete("/recipe/:id", auth, (request, response, next) => {
   Recipe.destroy({
     where: {
       id: request.params.id,
-      userId: request.user.dataValues.id
-    }
+      userId: request.user.dataValues.id,
+    },
   })
-    .then(numDeleted => {
+    .then((numDeleted) => {
       if (numDeleted) {
         response.status(204).end();
       } else {
